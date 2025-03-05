cdc0eb18623f1d773132ccacb70dea43da97da17
Sankalpa_01 <sankalpaanda@gmail.com>
2025-03-05 21:47:52 +0530

    inti

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new Schema
             lowercase: true,
             trim: true
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -53,10 +53,9 @@ const userSchema = new Schema
 )
 
 userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")){
-        return next();
-    }
-    this.password = bcrypt.hash(this.password, 10)
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })// yaha pe context pata hona bahaut jaruri hai aur normal syntax of callback me context pata nahi hota so normal syntax use mat karna yaha pe
 
@@ -64,6 +63,34 @@ userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password)
 }
 
+// both are jwt tokens just how to use differs in both
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 // jwt is a bearer-token : yeh token jiske pass hai uske pass data bhej deta hai backend
 
 
