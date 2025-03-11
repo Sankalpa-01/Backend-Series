c97b78138893946613f81e4832c13bc942ec854a
Sankalpa_01 <sankalpaanda@gmail.com>
2025-03-11 09:38:01 +0530

    init

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import connectDB from "./db/index.js" ;
 import {app} from './app.js'
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 // ese hum error ko nikal sakte hai
@@ -16,6 +16,7 @@ connectDB()
 })
 .catch((err) => {
     console.log("MONGO db connection failed !!!", err);
+    process.exit(1);
 })
 
 /*
